6c8f20db06ce44cd2945b38403cabd9c7551c7d3
rocku27 <rocku27@rocku27>
2014-09-08 11:21:30 +0530

    By Nirali

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo 'Rahul Patel';
+        echo 'Nirali Patel';
         ?>
     </body>
 </html>
